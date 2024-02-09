83a2d352bf1d54f28215b6c1cd9a4e6424c8cbd9
Omdeshmukh9 <omdesh89@gmail.com>
2024-02-09 21:30:39 +0530

    final commit

--- app/src/main/java/com/kazimasum/qrdemo/MainActivity.java
+++ app/src/main/java/com/kazimasum/qrdemo/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             scanbtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
                     startActivity(new Intent(getApplicationContext(),scannerView.class));
                 }
 
